95902c50bfd076208e73fb55e62133154b2dcd87
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-04-13 02:57:17 -0700

    person set

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -6,6 +6,7 @@ import Support from "./components/Support";
 import CG from "./components/CG";
 import Special from "./components/Special";
 import Character from "./components/Character";
+import Person from "./components/Person";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/cgs" element={<CG />} />
         <Route path="/special" element={<Special />} />
         <Route path="/character" element={<Character />} />
+        <Route path="/person/:id" element={<Person />} />
       </Routes>
     </BrowserRouter>
   );
